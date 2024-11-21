034f90891c7c1205bc397e5ce7f1a34825986463
HASHHman <SAHHH.ir@gmail.com>
2024-11-21 18:33:47 +0330

    Declare 3 vars and write the operate function

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let firstNum, secondNum, operator;
+
+function operate(firstNum, secondNum, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstNum, secondNum);
+      break;
+    case "-":
+      return subtract(firstNum, secondNum);
+      break;
+    case "*":
+      return multiply(firstNum, secondNum);
+      break;
+    case "/":
+      return divide(firstNum, secondNum);
+      break;
+  }
+}
